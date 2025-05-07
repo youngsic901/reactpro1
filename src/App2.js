76a95729be5a072d3198ac8f216a66c5b3cc91fe
youngsic901 <youngsic901@gmail.com>
2025-05-07 18:08:01 +0900

    2025.05.07 react test

---
+++ src/App2.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App2() {
+    let test = 313131;
+    return(
+        <div className='test'>
+            <h5>testData = {test}</h5>
+        </div>
+    );
+}
+
+export default App2;
